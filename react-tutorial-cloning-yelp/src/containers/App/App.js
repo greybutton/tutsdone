01bcc4c329593dd37b88bbe3845df2844b399524
greybutton <greybutton@yandex.com>
2017-12-19 22:44:58 +0800

    step "Creating a Sidebar"

--- react-tutorial-cloning-yelp/src/containers/App/App.js
+++ react-tutorial-cloning-yelp/src/containers/App/App.js
@@ -1,29 +1,31 @@
 import React, { PropTypes } from 'react';
-import ReactDOM from 'react-dom'
 import { Router } from 'react-router';
 
-import styles from './styles.module.css'
-
 class App extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
   static propTypes = {
-    routes: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired
-  }
+    history: PropTypes.object.isRequired,
+    routes: PropTypes.element.isRequired
+  };
 
-  // class getter
   get content() {
-    return (<Router
-        routes={this.props.routes}
-        history={this.props.history} />)
-  }
-
-  render() {
     return (
-      <div style={ { height: '100%' } }>
-        {this.content}
-      </div>
+      <Router
+        routes={this.props.routes}
+        history={this.props.history} />
     )
   }
+
+  render () {
+     return (
+       <div style={{ height: '100%' }}>
+         {this.content}
+       </div>
+     )
+   }
 }
 
-module.exports = App;
+export default App;
--- react-tutorial-cloning-yelp/src/views/Main/Container.js
+++ react-tutorial-cloning-yelp/src/views/Main/Container.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
 import {searchNearby} from 'utils/googleApiHelpers'
+import Header from 'components/Header/Header'
+
+import styles from './styles.module.css'
 
 export class Container extends React.Component {
   constructor(props) {
@@ -31,14 +34,17 @@ export class Container extends React.Component {
   render() {
     return (
       <div>
-        Hello from the container
         <Map
           visible={false}
           onReady={this.onReady.bind(this)}
-          google={this.props.google}>
+          google={this.props.google}
+          className={styles.wrapper}>
+          <Header />
+          <div className={styles.content}>
           {this.state.places.map(place => {
             return (<div key={place.id}>{place.name}</div>)
           })}
+          </div>
         </Map>
       </div>
     )
