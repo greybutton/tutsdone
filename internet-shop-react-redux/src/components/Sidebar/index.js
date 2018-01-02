dfe72520cdc3bd32a78cd69a9a11d0509e73316c
greybutton <greybutton@yandex.com>
2018-01-02 17:51:10 +0800

    done step #9

---
+++ internet-shop-react-redux/src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import BasketCart from '../BasketCart'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <BasketCart />
+    </div>
+  )
+}
+
+export default Sidebar
