2039b012b2c84024a6a2e30695f9558c1b35ae27
greybutton <greybutton@yandex.com>
2018-01-03 13:11:28 +0800

    done step 12

--- internet-shop-react-redux/src/components/Sidebar/index.js
+++ internet-shop-react-redux/src/components/Sidebar/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import BasketCart from '../BasketCart'
 import Search from '../Search'
+import Categories from '../Categories'
 
 const Sidebar = () => {
   return (
     <div>
       <BasketCart />
       <Search />
+      <Categories />
     </div>
   )
 }
