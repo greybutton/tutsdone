03fdfc70cca2cc7ec57a452afc9a43913423fd3d
greybutton <greybutton@yandex.com>
2018-01-15 23:13:40 +0800

    add redux

--- build-an-image-gallery-using-redux-saga/src/Gallery.js
+++ build-an-image-gallery-using-redux-saga/src/Gallery.js
@@ -1,42 +1,12 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
-const flickrImages = [
-  "https://farm2.staticflickr.com/1553/25266806624_fdd55cecbc.jpg",
-  "https://farm2.staticflickr.com/1581/25283151224_50f8da511e.jpg",
-  "https://farm2.staticflickr.com/1653/25265109363_f204ea7b54.jpg",
-  "https://farm2.staticflickr.com/1571/25911417225_a74c8041b0.jpg",
-  "https://farm2.staticflickr.com/1450/25888412766_44745cbca3.jpg"
-];
+import  * as GalleryActions from './actions.js';
 
-export default class Gallery extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: [],
-      selectedImage: null,
-    }
-  }
-  componentDidMount() {
-    const API_KEY = 'a46a979f39c49975dbdd23b378e6d3d5';
-    const API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.interestingness.+getList&api_key=${API_KEY}&format=json&nojsoncallback=1&per_page=5`;+
-
-    fetch(API_ENDPOINT).then((response) => {
-      return response.json().then((json) => {
-        const images = json.photos.photo.map(({farm, server, id, secret}) => { 
-            return `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
-        });
-
-        this.setState({images, selectedImage: images[0]});
-      })
-    })
-  }
-  handleThumbClick(selectedImage) {
-    this.setState({
-      selectedImage
-    })
-  }
+export class Gallery extends Component {
   render() {
-    const {images, selectedImage} = this.state;
+    const {images, selectedImage, selectImage} = this.props;
     return (
       <div className="image-gallery">
         <div className="gallery-image">
@@ -46,7 +16,7 @@ export default class Gallery extends Component {
         </div>
         <div className="image-scroller">
           {images.map((image, index) => (
-            <div key={index} onClick={this.handleThumbClick.bind(this, image)}>
+            <div key={index} onClick={() => selectImage(image)}>
               <img src={image}/>
             </div>
           ))}
@@ -55,3 +25,17 @@ export default class Gallery extends Component {
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    images: state.images,
+    selectedImage: state.selectedImage,
+  }
+}
+
+function mapActionCreatorsToProps(dispatch) {
+  return bindActionCreators(GalleryActions, dispatch);
+}
+
+
+export default connect(mapStateToProps, mapActionCreatorsToProps)(Gallery)
