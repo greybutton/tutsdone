e858113d7d869bd0f64212a027840c723042149b
greybutton <greybutton@yandex.com>
2018-02-09 23:19:08 +0800

    add todoitem component

--- redux-todomvc/src/components/TextInpu.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class TextInput extends React.PureComponent {
-  render() {
-    return <input className="edit"
-                  autoFocus={true}
-                  type="text" />
-  }
-};
