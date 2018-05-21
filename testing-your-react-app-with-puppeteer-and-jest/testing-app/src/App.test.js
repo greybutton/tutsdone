806cff4aad39054ba1c0029ef29e96d98b23c350
greybutton <greybutton@yandex.com>
2018-05-21 11:13:53 +0800

    init

---
+++ testing-your-react-app-with-puppeteer-and-jest/testing-app/src/App.test.js
@@ -0,0 +1,25 @@
+import puppeteer from "puppeteer";
+
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,
+    slowMo: 250,
+    devtools: true,
+  }
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
+}
+
+describe('on page load', () => {
+  test('h1 loads correctly', async() => {
+    let browser = await puppeteer.launch({})
+    let page = await browser.newPage()
+    
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400,
+      },
+      userAgent: ''
+    })
+  })
+})
