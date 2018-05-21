c539f8656361a4d935f54445790748b1fe24807f
greybutton <greybutton@yandex.com>
2018-05-21 12:21:57 +0800

    add html tests

--- testing-your-react-app-with-puppeteer-and-jest/testing-app/src/App.js
+++ testing-your-react-app-with-puppeteer-and-jest/testing-app/src/App.js
@@ -9,6 +9,14 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <nav className='navbar' role='navigation'>
+            <ul>
+              <li className="nav-li"><a href="#">Batman</a></li>
+              <li className="nav-li"><a href="#">Supermman</a></li>
+              <li className="nav-li"><a href="#">Aquaman</a></li>
+              <li className="nav-li"><a href="#">Wonder Woman</a></li>
+            </ul>
+          </nav>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- testing-your-react-app-with-puppeteer-and-jest/testing-app/src/App.test.js
+++ testing-your-react-app-with-puppeteer-and-jest/testing-app/src/App.test.js
@@ -1,4 +1,5 @@
-import puppeteer from "puppeteer";
+// import puppeteer from "puppeteer";
+const puppeteer = require('puppeteer');
 
 const isDebugging = () => {
   const debugging_mode = {
@@ -9,11 +10,23 @@ const isDebugging = () => {
   return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
 }
 
+let browser
+let page
+beforeAll(async () => { 
+  browser = await puppeteer.launch(isDebugging()) 
+  page = await browser.newPage() 
+  await page.goto('http://localhost:3000/') 
+  page.setViewport({ width: 500, height: 2400 })
+})
+
+afterAll(() => {     
+  if (isDebugging()) {         
+    browser.close()     
+  } 
+})
+
 describe('on page load', () => {
   test('h1 loads correctly', async() => {
-    let browser = await puppeteer.launch({})
-    let page = await browser.newPage()
-    
     page.emulate({
       viewport: {
         width: 500,
@@ -21,5 +34,15 @@ describe('on page load', () => {
       },
       userAgent: ''
     })
+
+    const html = await page.$eval('.App-title', e => e.innerHTML);
+    expect(html).toBe('Welcome to React');
+  })
+
+  test('nav loads correctly', async () => {
+    const navbar = await page.$eval('.navbar', el => el ? true : false)
+    const listItems = await page.$$('.nav-li')
+    expect(navbar).toBe(true)
+    expect(listItems.length).toBe(4)
   })
 })
