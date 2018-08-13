1c43eca0426efaa64c616810e43b26f2ad08e282
greybutton <greybutton@yandex.com>
2018-08-13 13:33:58 +0800

    Apollo Client in React Starter Project

--- react-apollo-client-example/src/App.js
+++ react-apollo-client-example/src/App.js
@@ -1,21 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      repositories(first: 20) {
+        edges {
+          node {
+            id
+            name
+            url
+            viewerHasStarred
+          }
+        }
+      }
+    }
   }
-}
+`;
+
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const UNSTAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    removeStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const App = () => (
+  <Query query={GET_REPOSITORIES_OF_ORGANIZATION}>
+    {({ data: { organization }, loading }) => {
+      if (loading || !organization) {
+        return <div>Loading ...</div>;
+      }
+
+      return (
+        <RepositoryList repositories={organization.repositories} />
+      );
+    }}
+  </Query>
+);
+
+const RepositoryList = ({ repositories }) => (
+  <ul>
+    {repositories.edges.map(({ node }) => {
+      return (
+        <li key={node.id}>
+          <a href={node.url}>{node.name}</a>{' '}
+          {node.viewerHasStarred ? <Unstar id={node.id} /> : <Star id={node.id} />}
+        </li>
+      );
+    })}
+  </ul>
+);
+
+const Star = ({ id }) => (
+  <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+    {starRepository => (
+      <button type="button" onClick={starRepository}>
+        Star
+      </button>
+    )}
+  </Mutation>
+);
+
+const Unstar = ({ id }) => (
+  <Mutation mutation={UNSTAR_REPOSITORY} variables={{ id }}>
+    {unstarRepository => (
+      <button type="button" onClick={unstarRepository}>
+        Unstar
+      </button>
+    )}
+  </Mutation>
+);
 
 export default App;
