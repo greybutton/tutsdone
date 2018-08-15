5eba740e4a967d724940b5410bdf4769ad4baf16
greybutton <greybutton@yandex.com>
2018-08-15 21:57:49 +0800

    Apollo Link State for Local State: Read Data

--- react-apollo-link-state-tutorial/src/App.js
+++ react-apollo-link-state-tutorial/src/App.js
@@ -1,21 +1,136 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      repositories(first: 20) {
+        edges {
+          node {
+            id
+            name
+            url
+            viewerHasStarred
+          }
+        }
+      }
+    }
   }
-}
+`;
+
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const UNSTAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    removeStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const GET_SELECTED_REPOSITORIES = gql`
+  query {
+    selectedRepositoryIds @client
+  }
+`;
+
+const App = () => (
+  <Query query={GET_REPOSITORIES_OF_ORGANIZATION}>
+    {({ data: { organization }, loading }) => {
+      if (loading || !organization) {
+        return <div>Loading ...</div>;
+      }
+
+      return (
+        <Repositories repositories={organization.repositories} />
+      );
+    }}
+  </Query>
+);
+
+const Repositories = ({ repositories }) => (
+  <Query query={GET_SELECTED_REPOSITORIES}>
+    {({ data: { selectedRepositoryIds } }) => (
+      <RepositoryList
+        repositories={repositories}
+        selectedRepositoryIds={selectedRepositoryIds}
+      />
+    )}
+  </Query>
+);
+
+
+const RepositoryList = ({
+  repositories,
+  selectedRepositoryIds,
+  toggleSelectRepository,
+}) => (
+  <ul>
+    {repositories.edges.map(({ node }) => {
+      const isSelected = selectedRepositoryIds.includes(node.id);
+
+      const rowClassName = ['row'];
+
+      if (isSelected) {
+        rowClassName.push('row_selected');
+      }
+
+      return (
+        <li className={rowClassName.join(' ')} key={node.id}>
+          <Select
+            id={node.id}
+            isSelected={isSelected}
+            toggleSelectRepository={toggleSelectRepository}
+          />{' '}
+          <a href={node.url}>{node.name}</a>{' '}
+          {node.viewerHasStarred ? <Unstar id={node.id} /> : <Star id={node.id} />}
+        </li>
+      );
+    })}
+  </ul>
+);
+
+const Select = ({ id, isSelected, toggleSelectRepository }) => (
+  <button
+    type="button"
+    onClick={() => toggleSelectRepository(id, isSelected)}
+  >
+    {isSelected ? 'Unselect' : 'Select'}
+  </button>
+);
+
+const Star = ({ id }) => (
+  <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+    {starRepository => (
+      <button type="button" onClick={starRepository}>
+        Star
+      </button>
+    )}
+  </Mutation>
+);
+
+const Unstar = ({ id }) => (
+  <Mutation mutation={UNSTAR_REPOSITORY} variables={{ id }}>
+    {unstarRepository => (
+      <button type="button" onClick={unstarRepository}>
+        Unstar
+      </button>
+    )}
+  </Mutation>
+);
 
 export default App;
---
+++ react-apollo-link-state-tutorial/src/resolvers.js
@@ -0,0 +1,4 @@
+export const defaults = {
+  selectedRepositoryIds: ['MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw=='],
+};
+export const resolvers = {};
